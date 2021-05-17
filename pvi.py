072f7a8093460545ab5f2ea8dcb01be7d1ea34a6
Willem Röpke <willem.ropke@vub.be>
2021-05-17 23:06:13 +0200

    Implement convergence strategy

--- pvi.py
+++ pvi.py
@@ -1,3 +1,5 @@
+import copy
+import math
 import gym
 import pandas as pd
 import numpy as np
@@ -8,7 +10,7 @@ register(
     id='RandomMOMDP-v0',
     entry_point='randommomdp:RandomMOMDP',
     reward_threshold=0.0,
-    kwargs={'nstates': 4, 'nobjectives': 2, 'nactions': 2, 'nsuccessor': 2, 'seed': 1}
+    kwargs={'nstates': 5, 'nobjectives': 3, 'nactions': 3, 'nsuccessor': 2, 'seed': 1}
 )
 
 env = gym.make('RandomMOMDP-v0')
@@ -20,6 +22,7 @@ transition_function = env._transition_function
 reward_function = env._reward_function
 
 gamma = 0.9  # Discount factor
+epsilon = 1  # How close we want to go to the PCS.
 
 
 def check_dominated(candidate, vectors):
@@ -39,14 +42,36 @@ def check_dominated(candidate, vectors):
         return False
 
 
+def check_converged(new_nd_vectors, old_nd_vectors):
+    """
+    This function checks if the PCS has converged.
+    :param new_nd_vectors: The updated PCS.
+    :param old_nd_vectors: The old PCS.
+    :return: Boolean whether the PCS has converged.
+    """
+    for new_set, old_set in zip(new_nd_vectors, old_nd_vectors):
+        min_epsilon = math.inf  # Initial value is positive infinity because we need the minimum.
+        for new_vec in new_set:
+            for old_vec in old_set:
+                diff = np.asarray(new_vec) - np.asarray(old_vec)
+                min_epsilon = min(min_epsilon, np.max(diff))  # We need the max difference.
+            if min_epsilon > epsilon:  # Early stop if the minimum epsilon for a vector is already above the threshold.
+                return False
+    return True
+
+
 def pvi():
     """
     This function will run the Pareto Value Iteration algorithm.
     :return: A set of non-dominated vectors per state in the MOMDP.
     """
     nd_vectors = [{tuple(np.zeros(num_objectives))} for _ in range(num_states)]  # Set non-dominated vectors to zero.
+    nd_vectors_update = copy.deepcopy(nd_vectors)
 
-    for run in range(10):  # We execute the algorithm a set amount of rounds.
+    run = 0  # For printing purposes.
+
+    while True:  # We execute the algorithm a set amount of rounds.
+        print(f'Value Iteration number: {run}')
         for state in range(num_states):  # Loop over all states.
             candidate_vectors = set()  # A set of new candidate non-dominated vectors for this state.
             new_nd_vectors = set()  # Initialise an empty set to hold the new non-dominated vectors for this state.
@@ -76,9 +101,13 @@ def pvi():
                 if not dominated:
                     new_nd_vectors.add(vec)  # If it is not, add it to the non-dominated set.
 
-            nd_vectors[state] = new_nd_vectors  # Update the non-dominated set.
+            nd_vectors_update[state] = new_nd_vectors  # Update the non-dominated set.
 
-    return nd_vectors
+        if check_converged(nd_vectors_update, nd_vectors):
+            return nd_vectors_update  # If converged, return the latest non-dominated vectors.
+        else:
+            nd_vectors = copy.deepcopy(nd_vectors_update)  # Else perform a deep copy an go again.
+            run += 1
 
 
 def save_vectors(nd_vectors):
