cb446829f95183bbdfa361ab5cc9ca954437017c
Roxana Radulescu <8026679+rradules@users.noreply.github.com>
2021-05-19 16:04:29 +0200

    fix get non dominated values method

--- pvi_fast.py
+++ pvi_fast.py
@@ -10,7 +10,7 @@ register(
     id='RandomMOMDP-v0',
     entry_point='randommomdp:RandomMOMDP',
     reward_threshold=0.0,
-    kwargs={'nstates': 4, 'nobjectives': 2, 'nactions': 2, 'nsuccessor': 3, 'seed': 1}
+    kwargs={'nstates': 5, 'nobjectives': 2, 'nactions': 2, 'nsuccessor': 3, 'seed': 1}
 )
 
 env = gym.make('RandomMOMDP-v0')
@@ -21,8 +21,8 @@ num_objectives = env._nobjectives
 transition_function = env._transition_function
 reward_function = env._reward_function
 
-gamma = 0.9  # Discount factor
-epsilon = 0.3 #0.1  # How close we want to go to the PCS.
+gamma = 0.8  # Discount factor
+epsilon = 0.1 # How close we want to go to the PCS.
 
 
 def get_non_dominated(candidates):
@@ -41,7 +41,7 @@ def get_non_dominated(candidates):
         # find all points not dominated by i
         # since points are sorted by coordinate sum
         # i cannot dominate any points in 1,...,i-1
-        nd[i+1:n] = (candidates[i+1:] >= candidates[i]).any(1)
+        nd[i+1:n] = (candidates[i+1:] > candidates[i]).any(1)
         # keep points non-dominated so far
         candidates = candidates[nd[:n]]
     return candidates
@@ -96,11 +96,13 @@ def pvi():
                             future_reward = np.array(curr_vec) + transition_prob * np.array(nd_vec)
                             new_future_rewards.add(tuple(future_reward))
 
-                    future_rewards = new_future_rewards  # Update the future rewards with the updated set.
+                    future_rewards = new_future_rewards # Update the future rewards with the updated set.
                 for future_reward in future_rewards:
                     value_vector = reward + gamma * np.array(future_reward)  # Calculate estimate of the value vector.
                     candidate_vectors.add(tuple(value_vector))
+            print(f'Candidates: {len(candidate_vectors)} for state {state}')
             nd_vectors_update[state] = get_non_dominated(candidate_vectors)  # Update the non-dominated set.
+            print(f'Nd values: {len(nd_vectors_update[state])} for state {state}')
 
         if check_converged(nd_vectors_update, nd_vectors):
             return nd_vectors_update  # If converged, return the latest non-dominated vectors.
