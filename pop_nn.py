8d84a47edc5924d57a0b6e0a3e2c460dea2fce72
Willem Röpke <willem.ropke@vub.be>
2021-12-15 08:22:30 +0100

    Rewrite pop nn to be more modular and add documentation

--- pop_nn.py
+++ pop_nn.py
@@ -1,94 +1,60 @@
-import gym
+import time
+import argparse
+
+import pandas as pd
+
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.utils.data import TensorDataset, DataLoader
-from gym.envs.registration import register
-import json
-import argparse
-import pandas as pd
+
 from sklearn.model_selection import train_test_split
 
-# Device configuration
-#device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-device = 'cpu' # Force CPU
-print(device)
 
-
-class POP_NN(nn.Module):
+class PopNetwork(nn.Module):
     """
-    Simple MLP model
-    :param d_layer: array with the layer size configuration
+    A pop following neural network.
     """
 
-    def __init__(self, d_layer):
-        super(POP_NN, self).__init__()
-        self.d_layer = d_layer
-        layer_list = [nn.Linear(d_layer[l], d_layer[l + 1]) for l in range(len(d_layer) - 1)]
-        self.lins = nn.ModuleList(layer_list)
+    def __init__(self, d_in, d_out, dropout):
+        super(PopNetwork, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(d_in, 1000),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(1000, 1000),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(1000, 1000),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(1000, 1000),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(1000, d_out),
+        )
 
     def forward(self, x):
-        x = x.view(-1, self.d_layer[0])
-        for layer in self.lins[:-1]:
-            x = F.relu(layer(x))
-        return self.lins[-1](x)
+        return self.layers(x)
 
 
-class POP_NN_SDST(nn.Module):
+def preprocess_data(data_file, num_objectives, batch, normalise=False, num_states=110, num_actions=4):
     """
-    Simple MLP model
-    :param d_layer: array with the layer size configuration
+    This function preprocesses the data and loads it into a train and validation loader.
+    :param data_file: The raw csv file path.
+    :param num_objectives: The number of objectives. Determines the targets for the neural network.
+    :param batch: The batch size to use in the data loaders.
+    :param normalise: Whether to normalise the data or not.
+    :param num_states: The number of states. Used for normalisation.
+    :param num_actions: The number of actions. Used for normalisation.
+    :return:
     """
+    data = pd.read_csv(data_file)  # Load training data.
 
-    def __init__(self, d_layer):
-        super(POP_NN_SDST, self).__init__()
-        self.d_layer = d_layer
-        layer_list = [nn.Linear(d_layer[l], d_layer[l + 1]) for l in range(len(d_layer) - 1)]
-        self.lins = nn.ModuleList(layer_list)
-
-    def forward(self, x):
-        x = x.view(-1, self.d_layer[0])
-        for layer in self.lins[:-1]:
-            x = F.leaky_relu(layer(x), 0.2)
-        return self.lins[-1](x)
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-states', type=int, default=110, help="number of states")
-    parser.add_argument('-obj', type=int, default=2, help="number of objectives")
-    parser.add_argument('-act', type=int, default=4, help="number of actions")
-    parser.add_argument('-suc', type=int, default=4, help="number of successors")
-    parser.add_argument('-seed', type=int, default=42, help="seed")
-    parser.add_argument('-method', type=str, default='PQL', help="method")
-    parser.add_argument('-novec', type=int, default=10, help="number of vectors")
-    parser.add_argument('-batch', type=int, default=8, help="batch size")
-    parser.add_argument('-epoch', type=int, default=3000, help="epochs")
-    parser.add_argument('-nnl', help='NN layer structure', type=lambda s: [int(item) for item in s.split(',')])
-
-    args = parser.parse_args()
-
-    path_data = f'results/'
-    file = f's{args.states}_a{args.act}_o{args.obj}_ss{args.suc}_seed{args.seed}_novec{args.novec}'
-
-    num_states = args.states
-    num_actions = args.act
-    num_objectives = args.obj
-    method = args.method
-    batch = args.batch
-    nnl = args.nnl
-
-    # Load training data
-    if num_states > 100:
-        data = pd.read_csv(f'{path_data}ND_normNN_{method}_{file}.csv')
-    else:
-        data = pd.read_csv(f'{path_data}ND_NN_{method}_{file}.csv')
-
-    # normalisation for states and actions
-    data['s'] = data['s'].values / num_states
-    data['ns'] = data['ns'].values / num_states
-    data['a'] = data['a'].values / num_actions
+    if normalise:  # Normalise states and actions.
+        data['s'] = data['s'].values / num_states
+        data['ns'] = data['ns'].values / num_states
+        data['a'] = data['a'].values / num_actions
 
     # Separate train from target (changes here is action should be a target)
     target = data[data.columns[-num_objectives:]].values
@@ -99,62 +65,122 @@ if __name__ == '__main__':
 
     # Create the data loaders
     train = TensorDataset(torch.tensor(X_train, dtype=torch.float32),
-                          torch.tensor(y_train, dtype=torch.float32))  # create your datset
+                          torch.tensor(y_train, dtype=torch.float32))
     val = TensorDataset(torch.tensor(X_val, dtype=torch.float32),
-                        torch.tensor(y_val, dtype=torch.float32))  # create your datset
+                        torch.tensor(y_val, dtype=torch.float32))
 
-    train_loader = DataLoader(train, shuffle=True, batch_size=batch)  # create your dataloader
-    val_loader = DataLoader(val, shuffle=True, batch_size=batch)  # create your dataloader
+    train_loader = DataLoader(train, shuffle=True, batch_size=batch)
+    val_loader = DataLoader(val, shuffle=True, batch_size=batch)
 
-    # input output size
-    d_in = num_objectives + 3
+    return train_loader, val_loader
+
+
+def load_network(num_objectives, dropout=0.5, checkpoint=None):
+    """
+    This function loads a neural network with the required parameters.
+    :param num_objectives: The number of objectives. This determines the number of inputs and outputs in the network.
+    :param dropout: The dropout rate in the network.
+    :param checkpoint: The file containing the last training checkpoint.
+    :return:
+    """
+    d_in = num_objectives + 3  # Three extra inputs because of state, action and next state.
     d_out = num_objectives
-    # init NN
-    nnl.insert(0, d_in)
-    nnl.append(d_out)
-    model = POP_NN(nnl).to(device)
-    #if num_states < 100:
-    #    model = POP_NN(nnl).to(device)
-    #else:
-    #    model = POP_NN_SDST(nnl).to(device)
+    model = PopNetwork(d_in, d_out, dropout)
+    if checkpoint is not None:
+        model.load_state_dict(torch.load(checkpoint))
+    return model
 
-    loss_function = nn.MSELoss().to(device)
+
+def train_pop_network(model, train_loader, val_loader, output_file, epochs=1000, predict_every=20):
+    """
+    This function trains a neural network.
+    :param model: The neural network.
+    :param train_loader: The loader for the training data.
+    :param val_loader: The loader for the validation data.
+    :param output_file: The filepath for saving the network parameters.
+    :param epochs: The total number of epochs to train for.
+    :param predict_every: Test on the validation set when this number of epochs has passed.
+    :return: The loss of the best model, the loss over time and the final model.
+    """
+    loss_function = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
-    n_epochs = args.epoch
-    predict_every = 20
-    min_valid_loss = 100.0
-    for epoch in range(n_epochs):
-        if epoch % 100 == 0:
-            print(epoch)
+    best_loss = float('inf')
+    loss_over_time = []  # We can save this for plotting purposes.
+    start = time.time()
+
+    for epoch in range(epochs):
+        print(f'Running epoch {epoch}')
+        epoch_loss = 0
+
         for batch_idx, (data, target) in enumerate(train_loader):
-            #if torch.cuda.is_available():
-             #   data, labels = data.cuda(), target.cuda()
             optimizer.zero_grad()
             output = model(data)
             loss = loss_function(output, target)
             loss.backward()
             optimizer.step()
-            train_loss = loss.data
-            # if batch_idx % predict_every == 0:
-            #    print(f'Train Epoch: {epoch}, Loss: {loss.data}')
+            epoch_loss += output.shape[0] * loss.item()
 
-        model.eval()  # Optional when not using Model Specific layer
-        b_num = 0
-        valid_loss = 0
-        for batch_idx, (data, target) in enumerate(val_loader):
-            #if torch.cuda.is_available():
-            #    data, target = data.cuda(), target.cuda()
-            output = model(data)
-            loss = loss_function(output, target)
-            valid_loss += loss.data
-            b_num += 1
-            # if batch_idx % predict_every == 0:
-            #    print(f'Epoch {epoch} \t\t Training Loss: {train_loss } '
-            #      f'\t\t Validation Loss: {valid_loss }')
-        valid_loss = valid_loss/b_num
-        if min_valid_loss > valid_loss:
-            print(f'Validation Loss Decreased({min_valid_loss:.6f}--->{valid_loss:.6f}) \t Saving The Model')
-            min_valid_loss = valid_loss
-            # Saving State Dict
-            torch.save(model.state_dict(), f'{path_data}ND_model_{batch}_{method}_{file}.pth')
+        total_loss = epoch_loss / len(train_loader.dataset)
+        loss_over_time.append(total_loss)
+        print(f'Training loss is: {total_loss}')
+
+        if epoch % predict_every == 0:
+            model.eval()
+            validation_loss = 0
+
+            for batch_idx, (data, target) in enumerate(val_loader):
+                output = model(data)
+                loss = loss_function(output, target)
+                validation_loss += output.shape[0] * loss.item()
+
+            total_validation_loss = validation_loss / len(val_loader.dataset)
+            if total_validation_loss < best_loss:
+                best_loss = total_validation_loss
+                torch.save(model.state_dict(), output_file)
+            print(f'Best validation loss is: {best_loss}')
+            model.train()
+
+        print('-------------------------------------------')
+
+    end = time.time()
+    elapsed_mins = (end - start) / 60.0
+    print(f'Finished training after {elapsed_mins} minutes')
+    print(f'Best model performance reached loss: {best_loss}')
+
+    return best_loss, loss_over_time, model
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-data', type=str, default='results/ND_NN_PVI_s110_a4_o2_ss4_seed42_novec10.csv', help='The path to the file containing the training data')
+    parser.add_argument('-output', type=str, default='results/model.pth', help='The file for saving the network.')
+    parser.add_argument('-checkpoint', type=str, default=None, help='A pretrained network to finetune.')
+    parser.add_argument('-states', type=int, default=110, help="number of states")
+    parser.add_argument('-act', type=int, default=4, help="number of actions")
+    parser.add_argument('-obj', type=int, default=2, help="number of objectives")
+    parser.add_argument('-normalise', type=bool, default=False, help='Normalise input data')
+    parser.add_argument('-epochs', type=int, default=3000, help="epochs")
+    parser.add_argument('-batch', type=int, default=8, help="batch size")
+    parser.add_argument('-dropout', type=float, default=0.5, help='Dropout rate for the neural network')
+
+    args = parser.parse_args()
+
+    # Extract arguments.
+    data_file = args.data
+    output_file = args.output
+    checkpoint_file = args.checkpoint
+    num_states = args.states
+    num_actions = args.act
+    num_objectives = args.obj
+    normalise = args.normalise
+    epochs = args.epochs
+    batch = args.batch
+    dropout = args.dropout
+
+    # Load the data and network and train it.
+    train_loader, val_loader = preprocess_data(data_file, num_objectives, batch, normalise=normalise, num_states=num_states, num_actions=num_actions)
+    model = load_network(num_objectives, dropout, checkpoint=checkpoint_file)
+    best_loss, loss_over_time, model = train_pop_network(model, train_loader, val_loader, output_file, epochs=epochs)
+
