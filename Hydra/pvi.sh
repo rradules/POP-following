97fa12f38d994fe5b47b38abaed73c15c9b9200e
Willem Röpke <willem.ropke@vub.be>
2021-12-20 08:51:20 +0100

    Jobscript for PVI on hydra

---
+++ Hydra/pvi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=pvi
+#SBATCH --time=1-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=64gb
+#SBATCH --mail-user=willem.ropke@vub.be
+#SBATCH --mail-type=ALL
+#SBATCH --output=logs/output-%A.out
+#SBATCH --error=logs/err-%A.err
+
+# Load the necessary modules.
+module load OpenAI-Gym/0.17.1-foss-2019b-Python-3.7.4
+module load SciPy-bundle/2021.05-foss-2021a
+
+# Navigate to the job directory.
+cd $VSC_HOME/POP-following
+
+# Run the neural network training procedure.
+python3 pvi.py
