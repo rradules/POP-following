3943ae193e6372cea6660bba1ed9e7d9dc541611
Willem Röpke <willem.ropke@vub.be>
2021-12-21 15:58:57 +0100

    Increased walltime and decreased number of iterations

--- Hydra/pvi.sh
+++ Hydra/pvi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=pvi
-#SBATCH --time=1-00:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem=64gb
 #SBATCH --mail-user=willem.ropke@vub.be
@@ -19,4 +19,4 @@ cd $VSC_HOME/POP-following
 # Run the neural network training procedure.
 python3 pvi.py \
 -novec 15 \
--num_iters 400
+-num_iters 200
